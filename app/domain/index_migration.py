cada6233da2a86797e505a2f870903903e5c87d6
DawidekZagajnik <krukdawid27@gmail.com>
2022-06-08 19:51:40 +0200

    added ID for IndexMigration class

--- app/domain/index_migration.py
+++ app/domain/index_migration.py
@@ -2,9 +2,11 @@ from pydantic import BaseModel
 from app.domain.reindex_endpoint import ReindexEndpoint
 from typing import Optional, List
 from app.domain.field_change import FieldChange
+from uuid import uuid4
 
 
 class IndexMigration(BaseModel):
+    id: Optional[str] = str(uuid4())
     name: str
     multi: bool
     endpoint: ReindexEndpoint
